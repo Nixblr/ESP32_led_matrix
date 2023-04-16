365dafca2e330ec26b091b54e90471d725a4f5a9
N1X <16139223+Nixblr@users.noreply.github.com>
2023-04-16 20:15:18 +0300

    16x16 matrix output added

--- main/main.c
+++ main/main.c
@@ -3,80 +3,99 @@
 #include <freertos/task.h>
 #include <esp_idf_version.h>
 #include "max7219.h"
+#include "stdint.h"
+#include "string.h"
 
 #ifndef APP_CPU_NUM
 #define APP_CPU_NUM PRO_CPU_NUM
 #endif
 
 #if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 0, 0)
-#define HOST    HSPI_HOST
+#define HOST HSPI_HOST
 #else
-#define HOST    SPI2_HOST
+#define HOST SPI2_HOST
 #endif
 
-#define CONFIG_EXAMPLE_CASCADE_SIZE 1
-#define CONFIG_EXAMPLE_SCROLL_DELAY 250
+#define CONFIG_EXAMPLE_CASCADE_SIZE 4
+#define CONFIG_EXAMPLE_SCROLL_DELAY 100
 #define CONFIG_EXAMPLE_PIN_NUM_MOSI 13
-#define CONFIG_EXAMPLE_PIN_NUM_CLK  14
-#define CONFIG_EXAMPLE_PIN_CS  25
+#define CONFIG_EXAMPLE_PIN_NUM_CLK 14
+#define CONFIG_EXAMPLE_PIN_CS 25
 
-static const uint64_t symbols[] = {
-    0x383838fe7c381000, // arrows
-    0x10387cfe38383800,
-    0x10307efe7e301000,
-    0x1018fcfefc181000,
-    0x10387cfefeee4400, // heart
-    0x105438ee38541000, // sun
+max7219_t dev;
 
-    0x7e1818181c181800, // digits
-    0x7e060c3060663c00,
-    0x3c66603860663c00,
-    0x30307e3234383000,
-    0x3c6660603e067e00,
-    0x3c66663e06663c00,
-    0x1818183030667e00,
-    0x3c66663c66663c00,
-    0x3c66607c66663c00,
-    0x3c66666e76663c00
-};
-static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CONFIG_EXAMPLE_CASCADE_SIZE;
+const uint8_t array[32] = {0x00,0x00,0x00,0x00,0x00,0x00,0x1b,0xec,
+						 0x24,0x12,0x40,0x01,0x48,0x09,0x5a,0x2d,
+						 0x6a,0x2b,0x08,0x08,0x09,0xc8,0x04,0x90,
+						 0x03,0xe0,0x00,0x00,0x00,0x00,0x00,0x00};
+
+void draw_screen(const uint8_t *frame)
+{
+	int i;
+	uint8_t tmp[8];
+	for (i = 0; i < 8; i++)
+	{
+		tmp[i] = frame[i*2];
+	}
+	max7219_draw_image_8x8(&dev, 24, tmp);
+	
+	for (i = 0; i < 8; i++)
+	{
+		tmp[i] = frame[i*2+1];
+	}
+	max7219_draw_image_8x8(&dev, 16, tmp);
+
+	for (i = 0; i < 8; i++)
+	{
+		tmp[i] = frame[i*2 + 16];
+	}
+	max7219_draw_image_8x8(&dev, 0, tmp);
+	for (i = 0; i < 8; i++)
+	{
+		tmp[i] = frame[i*2+16+1];
+	}
+	max7219_draw_image_8x8(&dev, 8, tmp);
+}
 
 void task(void *pvParameter)
 {
-    // Configure SPI bus
-    spi_bus_config_t cfg = {
-       .mosi_io_num = CONFIG_EXAMPLE_PIN_NUM_MOSI,
-       .miso_io_num = -1,
-       .sclk_io_num = CONFIG_EXAMPLE_PIN_NUM_CLK,
-       .quadwp_io_num = -1,
-       .quadhd_io_num = -1,
-       .max_transfer_sz = 0,
-       .flags = 0
-    };
-    ESP_ERROR_CHECK(spi_bus_initialize(HOST, &cfg, 1));
+	// Configure SPI bus
+	spi_bus_config_t cfg = {
+		.mosi_io_num = CONFIG_EXAMPLE_PIN_NUM_MOSI,
+		.miso_io_num = -1,
+		.sclk_io_num = CONFIG_EXAMPLE_PIN_NUM_CLK,
+		.quadwp_io_num = -1,
+		.quadhd_io_num = -1,
+		.max_transfer_sz = 0,
+		.flags = 0};
+	ESP_ERROR_CHECK(spi_bus_initialize(HOST, &cfg, 1));
 
-    // Configure device
-    max7219_t dev = {
-       .cascade_size = CONFIG_EXAMPLE_CASCADE_SIZE,
-       .digits = 0,
-       .mirrored = true
-    };
-    ESP_ERROR_CHECK(max7219_init_desc(&dev, HOST, MAX7219_MAX_CLOCK_SPEED_HZ, CONFIG_EXAMPLE_PIN_CS));
-    ESP_ERROR_CHECK(max7219_init(&dev));
+	// Configure device
+	dev.cascade_size = CONFIG_EXAMPLE_CASCADE_SIZE;
+	dev.digits = 0;
+	dev.mirrored = false;
 
-    size_t offs = 0;
-    while (1)
-    {
-        for (uint8_t c = 0; c < CONFIG_EXAMPLE_CASCADE_SIZE; c ++)
-            max7219_draw_image_8x8(&dev, c * 8, (uint8_t *)symbols + c * 8 + offs);
-        vTaskDelay(pdMS_TO_TICKS(CONFIG_EXAMPLE_SCROLL_DELAY));
+	ESP_ERROR_CHECK(max7219_init_desc(&dev, HOST, MAX7219_MAX_CLOCK_SPEED_HZ, CONFIG_EXAMPLE_PIN_CS));
+	ESP_ERROR_CHECK(max7219_init(&dev));
 
-        if (++offs == symbols_size)
-            offs = 0;
-    }
+	uint8_t screen[64];
+	int8_t offset = 0;
+
+	while (1)
+	{
+		memset(screen, 0, sizeof(screen));
+		memcpy((uint8_t*) screen + offset, array, 32);
+		offset -= 2;
+		draw_screen(screen);
+		offset -= 2;
+		if (offset < 0)
+		{
+			offset = 32;
+			vTaskDelay(pdMS_TO_TICKS(500));
+		}
+		vTaskDelay(pdMS_TO_TICKS(CONFIG_EXAMPLE_SCROLL_DELAY));
+	}
 }
-
-
 
 void app_main(void)
 {
